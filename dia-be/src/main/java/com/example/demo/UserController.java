de8eab6828b529351a24256243a1563b22fe112a
ivank <imalcekova@student.umb.sk>
2024-03-29 14:26:48 +0100

    Registrácia použivateľa + kontrola správnosti hesla a emailu pri registrácii

--- dia-be/src/main/java/com/example/demo/UserController.java
+++ dia-be/src/main/java/com/example/demo/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import com.example.demo.Service.CalendarDTO;
+import com.example.demo.Service.UserDTO;
 import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,4 +13,12 @@ import java.util.ArrayList;
 public class UserController {
     @Autowired
     UserService userService;
+    @PostMapping("/api/registration")
+    public Long UserRegistration(@RequestBody UserDTO userDTO){
+        return userService.userRegistration(userDTO);
+    }
+    @GetMapping("/api/registration/{id}")
+    public UserDTO GetRegistration(@PathVariable Long id){
+        return userService.GetRegistration(id);
+    }
 }
